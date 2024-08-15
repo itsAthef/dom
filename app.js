62d70f1ec9b3d107d2878de7c30211391afd3075
itsAthef <athefayubkhan2314@conestogac.on.ca>
2024-08-15 09:31:16 -0400

    keyboard events

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-let h3 = document.querySelector("h3");
+let inp = document.querySelector("input");
 
-h3.addEventListener("click", function () {
-  console.log("i was clciked hehe");
-});
-
-let box = document.querySelector(".box");
-
-box.addEventListener("mouseenter", function () {
-  console.log("You enter box");
+inp.addEventListener("keydown", function (event) {
+  console.log(event.code);
+  if (event.code === "ArrowUp") {
+    console.log("Character Moves Forward");
+  } else if (event.code === "ArrowDown") {
+    console.log("Character Moves Backward");
+  } else if (event.code === "ArrowLeft") {
+    console.log("Character Moves Left");
+  } else if (event.code === "ArrowRight") {
+    console.log("Character Moves Right");
+  }
 });
